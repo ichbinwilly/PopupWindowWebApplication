2abab63d848f2368832bbfc5576891cd7ef41792
ichbinwilly <mibhpwosiay@gmail.com>
2016-11-06 22:50:16 +0800

    first commit

---
+++ Controllers/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace PopupWindowWebApplication.Controllers
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PopupWindowWebApplication.Startup))]
+namespace PopupWindowWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
